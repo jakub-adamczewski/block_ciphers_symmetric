f9e4fd813e864ebb6aeef5c6856e21c6d1621c05
jakub-adamczewski <1999adamek@gmail.com>
2022-12-01 19:03:37 +0100

    fully working algorithms

--- 2_3_tasks.py
+++ 2_3_tasks.py
@@ -35,6 +35,18 @@ def cbc_encrypt_lib(input_path, output_path, key: bytes):
     return bytes(cipher.iv)
 
 
+def ecb_encrypt(input_path, output_path, key: bytes):
+    with open(input_path, 'r+') as file_in:
+        with open(output_path, "w+") as file_out:
+            data = '\n'.join(file_in.readlines())
+            input_bytes = pad(data.encode(errors='ignore'), AES.block_size)
+            blocks_num = int(len(input_bytes) / AES.block_size)
+            for i in range(0, blocks_num * AES.block_size, AES.block_size):
+                block_bytes = input_bytes[i: i + AES.block_size]
+                encrypted = ecb_encrypt_bytes(block_bytes, key)
+                file_out.write(encrypted.hex() + '\n')
+
+
 def cbc_encrypt(input_path, output_path, key: bytes, iv: bytes):
     iv_or_last_encrypted_block: bytes = iv
     with open(input_path, 'r+') as file_in:
@@ -46,7 +58,7 @@ def cbc_encrypt(input_path, output_path, key: bytes, iv: bytes):
                 block_bytes = input_bytes[i: i + AES.block_size]
                 encrypted = ecb_encrypt_bytes(byte_xor(iv_or_last_encrypted_block, block_bytes), key)
                 iv_or_last_encrypted_block = encrypted
-                file_out.write(encrypted.hex())
+                file_out.write(encrypted.hex() + '\n')
 
 
 def pcbc_encrypt(input_path, output_path, key: bytes, iv: bytes):
@@ -66,14 +78,28 @@ def pcbc_encrypt(input_path, output_path, key: bytes, iv: bytes):
                 encrypted = ecb_encrypt_bytes(second_xor_result, key)
                 iv_or_previous_encrypted_block = encrypted
                 previous_plaintext_block = block_bytes
-                file_out.write(encrypted.hex())
+                file_out.write(encrypted.hex() + '\n')
+
+
+def ecb_decrypt(input_path, output_path, key: bytes):
+    result = bytearray()
+    with open(input_path, 'r+') as file_in:
+        data = '\n'.join(file_in.readlines())
+        encrypted_bytes = bytes.fromhex(data)
+        blocks_num = int(len(encrypted_bytes) / AES.block_size)
+        for i in range(0, blocks_num * AES.block_size, AES.block_size):
+            block = encrypted_bytes[i: i + AES.block_size]
+            decrypted = ecb_decrypt_bytes(block, key)
+            result.extend(decrypted)
+        with open(output_path, "w+") as file_out:
+            file_out.write(unpad(bytes(result), AES.block_size).decode())
 
 
 def cbc_decrypt(input_path, output_path, key: bytes, iv: bytes):
     iv_or_last_encrypted_block: bytes = iv
     result = bytearray()
     with open(input_path, 'r+') as file_in:
-        data = ''.join(file_in.readlines())
+        data = '\n'.join(file_in.readlines())
         encrypted_bytes = bytes.fromhex(data)
         blocks_num = int(len(encrypted_bytes) / AES.block_size)
         for i in range(0, blocks_num * AES.block_size, AES.block_size):
@@ -89,7 +115,7 @@ def pcbc_decrypt(input_path, output_path, key: bytes, iv: bytes):
     iv_or_last_decrypted_block: bytes = iv
     result = bytearray()
     with open(input_path, 'r+') as file_in:
-        encrypted_bytes = bytes.fromhex(''.join(file_in.readlines()))
+        encrypted_bytes = bytes.fromhex('\n'.join(file_in.readlines()))
         previous_encrypted_block: Optional[bytes] = None
         blocks_num = int(len(encrypted_bytes) / AES.block_size)
         for i in range(0, blocks_num * AES.block_size, AES.block_size):
@@ -115,11 +141,33 @@ def test(msg: str = "Hello big world"):
     print(decrypted)
 
 
+def run_algorithm(file_nr: int, algorithm: str):
+    key = bytes.fromhex('77e557185e757a97fff61e6c5d2b44cc')
+    iv_bytes = bytes.fromhex('6dc65237be8e92311de34860f09812f4')
+    if algorithm == "ecb":
+        ecb_encrypt(f"files/example_{file_nr}.txt", f"files/{algorithm}/example_{file_nr}_encrypted.txt", key)
+        ecb_decrypt(f"files/{algorithm}/example_{file_nr}_encrypted.txt",
+                    f"files/{algorithm}/example_{file_nr}_decrypted.txt", key)
+    elif algorithm == "cbc":
+        cbc_encrypt(f"files/example_{file_nr}.txt", f"files/{algorithm}/example_{file_nr}_encrypted.txt", key,
+                    iv_bytes)
+        cbc_decrypt(f"files/{algorithm}/example_{file_nr}_encrypted.txt",
+                    f"files/{algorithm}/example_{file_nr}_decrypted.txt", key, iv_bytes)
+    elif algorithm == "pcbc":
+        pcbc_encrypt(f"files/example_{file_nr}.txt", f"files/{algorithm}/example_{file_nr}_encrypted.txt", key,
+                     iv_bytes)
+        pcbc_decrypt(f"files/{algorithm}/example_{file_nr}_encrypted.txt",
+                     f"files/{algorithm}/example_{file_nr}_decrypted.txt", key, iv_bytes)
+    else:
+        raise RuntimeError(f"Algorithm {algorithm} not implemented.")
+
+
 if __name__ == '__main__':
     key = bytes.fromhex('77e557185e757a97fff61e6c5d2b44cc')
     iv_bytes = bytes.fromhex('6dc65237be8e92311de34860f09812f4')
+    run_algorithm(file_nr=1, algorithm="pcbc")
     # iv_bytes = cbc_encrypt_lib("files/example_1.txt", "files/example_1_lib.txt", key)
     # cbc_encrypt("files/example_1.txt", "files/example_1_cbc_our.txt", key, iv_bytes)
     # cbc_decrypt("files/example_1_cbc_our.txt", "files/example_1_cbc_our_decr.txt", key, iv_bytes)
-    pcbc_encrypt("files/example_1.txt", "files/example_1_pcbc_our.txt", key, iv_bytes)
-    pcbc_decrypt("files/example_1_pcbc_our.txt", "files/example_1_pcbc_our_decr.txt", key, iv_bytes)
+    # pcbc_encrypt("files//example_1.txt", "files/example_1_encrypted.txt", key, iv_bytes)
+    # pcbc_decrypt("files/example_1_pcbc_our.txt", "files/example_1_pcbc_our_decr.txt", key, iv_bytes)
